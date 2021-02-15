19439b6ba0ff64aac84a29a57d76791e98288011
Sean Slater <sean@slaterfam.com>
2021-02-15 03:07:52 -0500

    Able to insert at least a prose book

--- spats_backend/database.py
+++ spats_backend/database.py
@@ -8,6 +8,7 @@ class Error(Exception):
 	def __init__(self, message):
 		self.message = message
 
+class InvalidInheritedAssetError(Error): pass
 class InvalidDecimalError(Error): pass
 class InvalidLengthError(Error): pass
 class InvalidSuuidError(Error): pass
@@ -73,32 +74,47 @@ class FieldParser:
 			raise OutOfBoundsError(f'"{value}" does not fall into required range of {min_value} and {max_value}')
 		return new_value
 
+	def _split_decimal(decimal, precision=None):
+		str_value = str(decimal)
+		point = str_value.count('.')
+		whole = '0'
+		fraction = '0'
+		if point == 1:
+			whole, fraction = str_value.split('.')
+		elif point == 0:
+			whole = str_value
+		else:
+			raise InvalidDecimalError(f'Decimal value "{str_value}" has too many decimal points')
+		if precision is not None and len(fraction) != precision:
+			fraction = fraction.ljust(precision, '0')[:precision]
+		if '-' == whole[0]:
+			fraction = '-' + fraction
+		return {'whole': int(whole), 'fraction': int(fraction)}
+
 	def decimal_field(value, params):
 		str_value = str(value)
 		precision = params.get('precision')
-		point = str_value.count('.')
-		if point == 1:
-			whole, fraction = str_value.split('.')
-			if precision is not None and len(fraction) != precision:
-				fraction = fraction.ljust(precision, '0')[:precision]
-			if '-' == whole[0]:
-				fraction = '-' + fraction
-			new_value = Decimal(int(whole), int(fraction))
-			min_value = Decimal(**params.get('min_value', {'whole': -math.inf, 'fraction': -math.inf}))
-			max_value = Decimal(**params.get('max_value', {'whole': math.inf, 'fraction': math.inf}))
-			if not (min_value <= new_value <= max_value):
-				raise OutOfBoundsError(f'"{value}" does not fall into required range of {min_value} and {max_value}')
-		elif point == 0:
-			new_value = Decimal(int(whole), 0)
-		else:
-			raise InvalidDecimalError(f'Decimal value "{value}" has too many decimal points')
-		return {'whole': new_value['whole'], 'fraction': new_value['fraction']}
+
+		new_dic = FieldParser._split_decimal(str_value, precision)
+		new_value = Decimal(**new_dic)
+
+		param_min = params.get('min_value')
+		min_dic = FieldParser._split_decimal(param_min, precision) if param_min is not None else {'whole': -math.inf, 'fraction': -math.inf}
+		min_value = Decimal(**min_dic)
+
+		param_max = params.get('max_value')
+		max_dic = FieldParser._split_decimal(param_max, precision) if param_max is not None else {'whole': math.inf, 'fraction': math.inf}
+		max_value = Decimal(**max_dic)
+
+		if not (min_value <= new_value <= max_value):
+			raise OutOfBoundsError(f'"{value}" does not fall into required range of {min_value} and {max_value}')
+		return {'whole': new_value.whole, 'fraction': new_value.fraction}
 
 	def date_field(value, params):
 		date_format = params.get('date_format', '%Y-%m-%d')
-		date_value = datetime.strptime(json[name], date_format).date()
-		min_value = params.get('min_value', date(datetime.MINYEAR, 1, 1))
-		max_value = params.get('max_value', date(datetime.MAXYEAR, 12, 31))
+		date_value = datetime.strptime(str(value), date_format)
+		min_value = params.get('min_value', datetime(MINYEAR, 1, 1))
+		max_value = params.get('max_value', datetime(MAXYEAR, 12, 31))
 		if not (min_value <= date_value <= max_value):
 			raise OutOfBoundsError(f'"{value}" does not fall into required range of {min_value} and {max_value}')
 		return date_value
@@ -207,8 +223,9 @@ class Database:
 						doc = {'_id': inherit}
 					res = self._get('asset', doc)
 					if res:
-						merged = merge_docs(src=res, dest=json)
-						json = merged
+						json = merge_docs(src=res, dest=json)
+					else:
+						raise InvalidInheritedAssetError(f'"{inherit}" is not a valid asset type')
 			res = self._insert_many('asset', json_list)
 			created = res.inserted_ids
 		return {'created': created}
