db136626ec9f126d0ad41a877696012d02a4ffba
Sean Slater <sean@slaterfam.com>
2021-02-23 22:24:07 -0500

    sort things

--- spats_backend/database.py
+++ spats_backend/database.py
@@ -21,6 +21,65 @@ class NoDocumentFound(Error): pass
 class RequiredAttributeError(Error): pass
 class UniqueAttributeNotUniqueError(Error): pass
 
+
+class TupleNoneCompare:
+	def __init__(self, x):
+		self.x = x
+
+	def _not_same(self, x, y):
+		return ((x is None and y is not None)
+			or (x is not None and y is None)
+			or (not isinstance(x, type(y))))
+
+	def __len__(self):
+		return len(self.x)
+
+	def __getitem__(self, y):
+		return self.x[y]
+
+	def __lt__(self, y):
+		x = self.x
+		len_x = len(x)
+		len_y = len(y)
+		for i in range(max(len_x, len_y)):
+			if (self._not_same(x[i], y[i])
+				or (len_x > i and len_y == i)
+				or ((x[i] is not None or y[i] is not None) and (x[i] > y[i]))):
+				return False
+			if ((len_x == i and len_y > i)
+				or ((x[i] is not None or y[i] is not None) and (x[i] < y[i]))):
+				return True
+		return False
+
+	def __le__(self, y):
+		return self.__lt__(y) or self.__eq__(y)
+
+	def __eq__(self, y):
+		x = self.x
+		if ((len(x) != len(y))
+			or self._not_same(x[0], y[0])
+			or (x[0] != y[0])
+			or self._not_same(x[1], y[1])
+			or (x[1] != y[1])):
+			return False
+		len_x = len(x)
+		len_y = len(y)
+		for i in range(2,len_x):
+			if (self._not_same(x[i], y[i])
+				or (x[i] != y[i])):
+				return False
+		return True
+
+	def __ne__(self, y):
+		return not self.__eq__(y)
+
+	def __gt__(self, y):
+		return not self.__lt__(y)
+
+	def __ge__(self, y):
+		return self.__gt__(y) or self.__eq__(y)
+
+
 class Database:
 	def __init__(self, app=None):
 		self.app = app
@@ -155,6 +214,10 @@ class Database:
 				res[_id] = json
 		return res
 
+	def _symbolic_all(self, type_):
+		docs = self._get_many(type_)
+		return sorted(docs, key=lambda doc: doc.get('name'))
+
 	def _symbolic_get(self, type_, value):
 		res = {}
 		try:
@@ -267,6 +330,19 @@ class Database:
 			res['fields'][key] = self.field_parser.decode(field_type, value, field_params)
 		return res
 
+	def _material_sort_key(self, symbolic_res):
+		def helper(doc):
+			symbolic = symbolic_res[doc['type']]
+			primary_key = symbolic.get('primary')
+			secondary_key = symbolic.get('secondary')
+			tertiary_keys = symbolic.get('tertiary', [])
+			fields = doc['fields']
+			vals = [fields.get(primary_key), fields.get(secondary_key)]
+			if tertiary_keys:
+				vals.extend([ fields.get(key) for key in tertiary_keys ])
+			return TupleNoneCompare(vals)
+		return helper
+
 	def _material_all(self, type_, symbolic_type, symbolic_lookup=None):
 		material_res = []
 		symbolic_res = {}
@@ -292,6 +368,7 @@ class Database:
 				res['type'] = type_
 
 		symbolic_res = self._to_id_dic(symbolic_res)
+		raw_res.sort(key=self._material_sort_key(symbolic_res))
 		for raw in raw_res:
 			raw_type = raw['type']
 			symbolic_cur = symbolic_res[raw_type]
@@ -491,7 +568,7 @@ class Database:
 
 
 	def asset_all(self):
-		return self._get_many('asset')
+		return self._symbolic_all('asset')
 
 	def asset_get(self, value):
 		return self._symbolic_get('asset', value)
@@ -521,7 +598,7 @@ class Database:
 		return self._material_delete('thing', json_list)
 
 	def combo_all(self):
-		return self._get_many('combo')
+		return self._symbolic_all('combo')
 
 	def combo_get(self, value):
 		return self._symbolic_get('combo', value)
